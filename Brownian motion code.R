145a04192d35c5dc289599c345dce8de891f4923
MathGirl <poeticphoebe@gmail.com>
2017-07-05 19:55:51 +0100

    Add files via upload

--- Brownian motion code.R
+++ Brownian motion code.R
@@ -1,11 +1,13 @@
 
-# Last updated: Jul 3 2017 ------------------------------------------------
+# Last updated: Jul 5 2017 ------------------------------------------------
 
 ### 1 Simulating CIR SDE Sample Paths ###
 
-x0 = runif(1)
+x0 = runif(1) # starting point of y_t
+t = 1000 # number of time points
+
 my.fun = function() {
-  n = 500
+  n = t
   
   k = runif(1)
   nu = runif(1)
@@ -20,16 +22,44 @@ my.fun = function() {
 }
 
 
-npath <- 400
-W.t.paths <- replicate(npath, my.fun())
+npath = 400 # number of sample paths
+W.t.paths = replicate(npath, my.fun())
 
-n = 1:500
+n = 1:t
 
-### Plot of the paths
-layout(cbind(1, 2), width = c(1, 1/2)) # plot layout
+### Plot of the paths ###
 yran <- range(W.t.paths) # plot range
-opar <- par(mar = c(4.5, 4.2, 1, 0.5)) # reduce space around plot 1, especially to the right
 plot(n, W.t.paths[,1], type = "l", ylim = yran, # plot the first path
-     xlab = "Time steps", ylab = expression("Yt that follows a CIR process"))
+     xlab = "time t", ylab = expression("Y(t)"),
+     main = "CIR Process paths")
 for(k in 2:npath) # plot the other paths
   lines(n, W.t.paths[,k], col = adjustcolor("black", alpha.f = 1/(1+npath/14)))
+
+### 2 Simulation of Poisson processes ###
+
+lambda <- 0.05
+tMax <- 100
+
+## find the number 'n' of exponential r.vs required by imposing that
+## Pr{N(t) <= n} <= 1 - eps for a small 'eps'
+n <- qpois(1 - 1e-8, lambda = lambda * tMax)
+
+## simulate exponential interarrivals
+X <- rexp(n = n, rate = lambda)
+S <- c(0, cumsum(X))
+plot(x = S, y = 0:n, type = "s", xlim = c(0, tMax)) 
+
+## several paths?
+npath <- 50
+## simulate exponential interarrivals
+X <- matrix(rexp(n * npath, rate = lambda), ncol = npath,
+            dimnames = list(paste("S", 1:n, sep = ""), paste("samp", 1:npath)))
+## compute arrivals, and add a fictive arrival 'T0' for t = 0
+S <- apply(X, 2, cumsum)
+S <- rbind("T0" = rep(0, npath), S)
+head(S)
+## plot using steps
+matplot(x = S, y = 0:n, type = "s", col = "darkgray",
+        xlim = c(0, tMax),
+        main = "Homogeneous Poisson Process paths", xlab = "time t", 
+        ylab = "N(t)")
